8f02fe09ec53c1491dcd07eb020c18bf1d1eac16
2995506210101 <160443912+2995506210101@users.noreply.github.com>
2024-03-19 01:21:51 -0600

    Tarea 6

---
+++ Tarea 6/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim notas(4) As Double
+        Dim suma As Double = 0
+        For i As Integer = 0 To 4
+            Console.Write("Ingrese la nota " & (i + 1) & ": ")
+            notas(i) = Double.Parse(Console.ReadLine())
+            suma += notas(i)
+        Next
+        Dim promedio As Double = suma / 5
+        Console.WriteLine("El promedio es: " & promedio)
+        Console.WriteLine("Presione cualquier tecla para cerrar el programa")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
