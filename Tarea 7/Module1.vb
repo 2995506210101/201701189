07739f16e0101fb5b0e50a7f6bf76df339ae35ea
2995506210101 <160443912+2995506210101@users.noreply.github.com>
2024-03-20 23:45:24 -0600

    Tarea 7

---
+++ Tarea 7/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim m, n, i, j As Integer
+        Dim A(2, 2) As Integer
+        Console.Write(vbLf & "Enter The Matrix Elements any two: ")
+        For i = 0 To 2 - 1
+            For j = 0 To 2 = 1
+                A(i, j) = Convert.ToInt16(Console.ReadLine())
+            Next
+        Next
+        Console.Clear()
+        Console.WriteLine(vbLf & "Matrix A: ")
+        For i = 0 To 2 - 1
+            For j = 0 To 2 - 1
+                Console.Write(vbLf & "{0}", A(i, j))
+            Next
+            Console.WriteLine(" ")
+        Next
+        Console.WriteLine(vbLf & "Transpose Matrix: ")
+        For i = 0 To 2 - 1
+            For j = 0 To 2 - 1
+                Console.Write(vbLf & "{0}", A(j, i))
+            Next
+            Console.WriteLine("Presione enter para salir ")
+            Console.ReadLine()
+        Next
+    End Sub
+
+End Module
---
+++ Tarea 7/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Tarea 7")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Tarea 7")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("e0c0e82c-958d-41b2-9ad8-db7feb7511a2")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+' utilizando el carácter "*", como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
